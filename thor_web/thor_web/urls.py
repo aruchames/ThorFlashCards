526db902af1ba9dfdede0ff5339ffae53745085a
eddielu <eddielu@github.com>
2015-03-29 17:28:35 +0000

    Begin urls redirecting

--- thor_web/thor_web/urls.py
+++ thor_web/thor_web/urls.py
@@ -7,5 +7,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', include('thor_frontend.urls')),
+    url(r'^', include('thor_frontend.urls')),
 )
