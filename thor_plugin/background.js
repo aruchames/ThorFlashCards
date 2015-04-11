312f8929a9f3d9547cd342548288312b31e3f857
Andrew Ruchames <ruchames@princeton.edu>
2015-04-11 00:27:59 +0000

    Added form to send flashcard information, modified manifest to add content scripts, changed a few small things in the style of popup.

--- thor_plugin/background.js
+++ thor_plugin/background.js
@@ -6,14 +6,17 @@ function onClickHandler(info, tab) {
 };
 
 chrome.contextMenus.onClicked.addListener(onClickHandler);
-
+chrome.contextMenus.onclicked.
 
 // Set up context menu tree at install time.
 chrome.runtime.onInstalled.addListener(function() {
-
+    
     var context = "selection";
     var title = "Use ThorFC to translate \"%s\"";
     var id = chrome.contextMenus.create({"title": title, "contexts":[context],
                                          "id": "context" + context});
+    
+    // Set up Login on install
+    
 
 });
