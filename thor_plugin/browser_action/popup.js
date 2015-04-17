0c810d85f4bdb797371ad52651487a07aaa1c51d
eddielu <ejlu@princeton.edu>
2015-04-17 17:16:00 -0400

    Login/Register browser action working

--- thor_plugin/browser_action/popup.js
+++ thor_plugin/browser_action/popup.js
@@ -4,19 +4,22 @@ $(document).ready(function () {
     var xhr = new XMLHttpRequest();
     xhr.open("GET", deckURL, false);
     xhr.send();
+	$("#login").children("input").on('click', function() {chrome.tabs.create({"url":"http://www.thorfc.com/login/"})});
+	$("#register").children("input").on('click', function() {chrome.tabs.create({"url":"http://www.thorfc.com/register/"})})
+
 
     //If the user is not registered, load the view to send the user to login
     if (JSON.parse(xhr.responseText).hasOwnProperty("detail")){
     	//load login view
     	console.log("User not logged in");
-    	debugger;
+    	//debugger;
     }
     // Load users decks in a scroll down menu and choose the deck that will be submit to.
     else{
-    	debugger;
-    	console.log(xhr);
+    	//debugger;
+    	//console.log(xhr);
     	var login = document.getElementById("login");
-    	var deckViewParent = login.parentNode;
+    	var body = login.parentNode;
     	login.style.display="none";
     	var register = document.getElementById("register");
     	register.style.display="none";
@@ -28,11 +31,8 @@ $(document).ready(function () {
 
     	var deckView = document.createElement("deckView");
     	deckView.innerHTML="<div>User:<div id ='user'>ruchames</div>Decks:  <select name='decks'><option value='Chinese'> Chinese </option><option value='French'>French</option><option value='COS'>COS</option></select></div>";
-		deckViewParent.appendChild(deckView);
-
-
-
+		body.appendChild(deckView);
     }
-    console.log(decks);
+    //console.log(decks);
 
 });
