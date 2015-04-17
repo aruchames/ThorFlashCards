85783122c6e1dcd33ff75d64439c8f4e29498f43
eddielu <ejlu@princeton.edu>
2015-04-17 17:00:29 -0400

    Ajax in browser action working

--- thor_plugin/browser_action/popup.js
+++ thor_plugin/browser_action/popup.js
@@ -1,5 +1,6 @@
+$(document).ready(function () {
 	var deckURL = "https://www.thorfc.com/api/users/me";
-    
+
     var xhr = new XMLHttpRequest();
     xhr.open("GET", deckURL, false);
     xhr.send();
@@ -10,28 +11,28 @@
     	console.log("User not logged in");
     	debugger;
     }
-    // Load users decks in a scroll down menu and choose the deck that will be submit to. 
+    // Load users decks in a scroll down menu and choose the deck that will be submit to.
     else{
     	debugger;
     	console.log(xhr);
-    	var login = document.getElementByID("login");
-    	var deckViewParent = login.parent;
-    	login.style.visibility="hidden";
-    	var register = document.getElementByID("register");
-    	register.style.visibility="hidden";
-    	var response = raw.responseText;
+    	var login = document.getElementById("login");
+    	var deckViewParent = login.parentNode;
+    	login.style.display="none";
+    	var register = document.getElementById("register");
+    	register.style.display="none";
+    	var response = xhr.responseText;
     	var userData = JSON.parse(response);
-    
+
    	 	var username = userData.username;
     	var decks = userData.decks;
 
     	var deckView = document.createElement("deckView");
     	deckView.innerHTML="<div>User:<div id ='user'>ruchames</div>Decks:  <select name='decks'><option value='Chinese'> Chinese </option><option value='French'>French</option><option value='COS'>COS</option></select></div>";
 		deckViewParent.appendChild(deckView);
-		
+
 
 
     }
     console.log(decks);
 
-
+});
