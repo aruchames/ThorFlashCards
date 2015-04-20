c8d4d3e0217dee10e1fb6bb69ff8235780646740
eddielu <ejlu@princeton.edu>
2015-04-20 14:15:26 -0400

    Fix bug: Button value

--- thor_plugin/content2.js
+++ thor_plugin/content2.js
@@ -95,7 +95,7 @@ function onSelect(e) {
     }
     else {
         var translateCall = response.trans[0];
-        var htmlFrag = "<div id='card'><h5>Original Text:</h5><div id='thorFCfront'>" + result + "</div><h5>Translated Text:</h5> <div id='thorFCback'>"+ translateCall + "</div><br> <button value='Make Card!' id='thorfcbutton'></div>";
+        var htmlFrag = "<div id='card'><h5>Original Text:</h5><div id='thorFCfront'>" + result + "</div><h5>Translated Text:</h5> <div id='thorFCback'>"+ translateCall + "</div><br> <button id='thorfcbutton'>Make Card!</button></div>";
     }
     bubbleDOM.innerHTML = htmlFrag;
     bubbleDOM.appendChild(thorFCdeckView);
